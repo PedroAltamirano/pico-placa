9e2619cc7db43447050893067e570f19aa162fcd
PedroAltamirano <pedroaal04@gmail.com>
2021-03-17 17:27:02 -0500

    inputs added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container">
+		<div class="form-group">
+			<label for="placa">Placa</label>
+			<input
+				type="text"
+				class="form-control"
+				name="placa"
+				id="placa"
+				placeholder="PCA9999"
+				v-model="placa_input"
+				min="6"
+				max="7"
+				:class="{ 'is-invalid': comprobar }"
+				@keyup.enter="calcular"
+			/>
+		</div>
+		
+
+		<div class="form-group">
+			<label for="fecha">Fecha</label>
+			<input type="date" class="form-control" name="fecha" id="fecha" v-model="fecha_input" @change="calcular" />
+		</div>
+
+		<button type="button" @click="calcular" class="btn btn-primary btn-block">Revisar placa</button>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	data() {
+		return {
+			placa_input: '',
+			fecha_input: '',
+		};
+	},
+	methods: {
+		calcular() {
+			console.log(this.placa_input);
+		},
+	},
+	computed: {
+		comprobar() {
+			return (
+				this.placa_input.length < 7 || this.placa_input.length > 7 || !(/[a-zA-Z]{3}[0-9]{4}/.test(this.placa_input))
+			);
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
