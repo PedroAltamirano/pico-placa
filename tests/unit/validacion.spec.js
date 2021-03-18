edc8c7c9493e331b3f03c45e835990cce2436f1d
PedroAltamirano <pedroaal04@gmail.com>
2021-03-18 14:54:04 -0500

    descripcion de tests

--- tests/unit/validacion.spec.js
+++ tests/unit/validacion.spec.js
@@ -22,21 +22,21 @@ describe('app validations work', () => {
 		expect(wrapper.vm.calcular()).toBeFalsy();
 	});
 
-	it('regex placa validation', () => {
+	it('placa par circula, hora', () => {
 		wrapper.find('#placa').setValue('2222');
 		wrapper.find('#fecha').setValue('2021-03-17');
 		wrapper.find('#hora').setValue('13:05');
 		expect(wrapper.vm.calcular()).toBeTruthy();
 	});
 
-	it('regex placa validation', () => {
+	it('placa par no circula, hora', () => {
 		wrapper.find('#placa').setValue('2222');
 		wrapper.find('#fecha').setValue('2021-03-17');
 		wrapper.find('#hora').setValue('07:05');
 		expect(wrapper.vm.calcular()).toBeFalsy();
 	});
 
-	it('regex placa validation', () => {
+	it('placa par circula, dia', () => {
 		wrapper.find('#placa').setValue('2222');
 		wrapper.find('#fecha').setValue('2021-03-18');
 		wrapper.find('#hora').setValue('13:05');
