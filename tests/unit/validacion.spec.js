dbe1da83b04d45e3ac0d42d4d8d027087d30d705
PedroAltamirano <pedroaal04@gmail.com>
2021-03-18 14:49:44 -0500

    prueba de funcionalidad

---
+++ tests/unit/validacion.spec.js
@@ -0,0 +1,45 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App.vue';
+
+describe('app validations work', () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallowMount(App);
+	});
+
+	it('placa validation', () => {
+		expect(wrapper.vm.calcular()).toBeFalsy();
+	});
+
+	it('fecha validation', () => {
+		wrapper.find('#placa').setValue('2222');
+		expect(wrapper.vm.calcular()).toBeFalsy();
+	});
+
+	it('hora validation', () => {
+		wrapper.find('#placa').setValue('2222');
+		wrapper.find('#fecha').setValue('22223');
+		expect(wrapper.vm.calcular()).toBeFalsy();
+	});
+
+	it('regex placa validation', () => {
+		wrapper.find('#placa').setValue('2222');
+		wrapper.find('#fecha').setValue('2021-03-17');
+		wrapper.find('#hora').setValue('13:05');
+		expect(wrapper.vm.calcular()).toBeTruthy();
+	});
+
+	it('regex placa validation', () => {
+		wrapper.find('#placa').setValue('2222');
+		wrapper.find('#fecha').setValue('2021-03-17');
+		wrapper.find('#hora').setValue('07:05');
+		expect(wrapper.vm.calcular()).toBeFalsy();
+	});
+
+	it('regex placa validation', () => {
+		wrapper.find('#placa').setValue('2222');
+		wrapper.find('#fecha').setValue('2021-03-18');
+		wrapper.find('#hora').setValue('13:05');
+		expect(wrapper.vm.calcular()).toBeTruthy();
+	});
+});
